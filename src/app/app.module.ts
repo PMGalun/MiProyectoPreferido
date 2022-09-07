6b322db3a06dc0569d335329fd0c32d22fd3eb74
PMG <pattoyo01@gmail.com>
2022-09-07 07:28:42 -0300

    se completo acerca de

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { RedessocialesComponent } from './componentes/redessociales/redessociales.component';
+import { HyHComponent } from './componentes/hy-h/hy-h.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    RedessocialesComponent,
+    HyHComponent,
+    EducacionComponent,
+    BannerComponent,
+    LogoAPComponent
   ],
   imports: [
     BrowserModule,
